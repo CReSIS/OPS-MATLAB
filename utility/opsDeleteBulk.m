4a7ca4d57bd70de3c3063b8f5b88c86d814c057d
Trey Stafford <treystaff@gmail.com>
2014-06-09 10:27:15 -0500

    Update to prevent accidental or unauthorized deletions

--- utility/opsDeleteBulk.m
+++ utility/opsDeleteBulk.m
@@ -8,17 +8,47 @@ function [status,data] = opsDeleteBulk(sys,param)
 %   sys: (string) sys name ('rds','accum','snow',...)
 %   param: structure with fields
 %     properties.season: (string) season name for given segments ('2011_Greenland_P3')
-%     OPTIONAL:
-%       properties.only_layer_points: (boolean) true deletes only layer points
-%       properties.segment = cell of segment name(s)
-%           {'20110331_01'}: deletes the given single segment
-%           {'20110331_01','20110331_02'}: deletes both given segments
+%     properties.only_layer_points: (boolean) true deletes only layer points
+%     properties.segment = cell of segment name(s)
+%         {''}: deletes all segments
+%         {'20110331_01'}: deletes the given single segment
+%         {'20110331_01','20110331_02'}: deletes both given segments
 %
 % Output:
 %   status: integer (0:Error,1:Success,2:Warning)
 %   data: Message indicating deletion status
 %
 % Author: Trey Stafford, Kyle Purdon
+
+% AUTHENTICATE THE USER
+[param,~,opsProfile] = opsAuthenticate(param,true);
+if ~opsProfile.bulkDeleteData
+    error('USER NOT AUTHORIZED TO BULK DELETE DATA');
+end
+
+% CONFIRM INPUTS BEFORE PROCEEDING 
+if strcmp(param.properties.segment{1},'')
+    segments = 'ALL';
+else
+    segments = sprintf(' %s;',param.properties.segment{:});
+end
+
+if param.properties.only_layer_points
+    layerPoints = 'True';
+else
+    layerPoints = 'False';
+end
+opsCmd;
+confirmParams = {sprintf('SERVER: \t %s',gOps.serverUrl),'',sprintf('SEASON NAME: \t %s',param.properties.season),'',...
+  sprintf('SYSTEM NAME: \t %s',sys),'',sprintf(' Only Delete Layer Points?: \t %s',layerPoints),'',...
+  sprintf(' SEGMENTS: \t %s',segments),''};
+
+confirmButton = questdlg(confirmParams,'Confirm Settings','YES:DELETE','NO:QUIT','NO:QUIT');
+
+switch confirmButton
+  case 'NO:QUIT'
+    error('PROCESS STOPPED BY USER.');
+end
 
 % CONSTRUCT THE JSON STRUCTURE
 jsonStruct = struct('properties',param.properties);
@@ -31,7 +61,6 @@ catch ME
 end
 
 % SEND THE COMMAND TO THE SERVER
-opsCmd;
 if gOps.profileCmd
     [jsonResponse,~] = opsUrlRead(strcat(gOps.serverUrl,'profile'),gOps.dbUser,gOps.dbPswd,...
         'Post',{'app' sys 'data' jsonStr 'view' 'bulkDelete'});
